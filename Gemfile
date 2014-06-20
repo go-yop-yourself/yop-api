75cf1f938fb4caa6c780347e02d08a31e1b63b78
Callum Stott <callum@pivotallabs.com>
2014-06-20 23:04:38 +0100

    Cert stuff yeah son :metal:

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.0.0'
 gem "sinatra"
 gem "thin"
 gem "sequel"
+gem "houston"
 
 group :development do
   gem "sqlite3"
--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sequel'
 
 DB = Sequel.connect("sqlite://db/yop.db")
+APN = Houston::Client.development
+APN.certificate = File.read("keys/aps_development.cer")
 
 post "/users" do
   DB[:users].insert(
@@ -12,7 +14,12 @@ post "/users" do
 end
 
 post "/notifications" do
-  
+  to_username = params[:username]
+  user = DB[:users].where(:username => to_username).first
+  notification = Houston::Notification.new(device: user[:device_token])
+  notification.alert = "YOP"
+
+  APN.push(notification)
 end
 
 get '/yop' do
